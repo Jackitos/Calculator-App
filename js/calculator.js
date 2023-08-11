60fa33477fbbf0649fc878fb84ca5d184f14f57c
Jackitos <tomjack.dev@gmail.com>
2023-08-11 12:28:45 -0300

    Explicación del código con comentarios

--- js/calculator.js
+++ js/calculator.js
@@ -1,6 +1,7 @@
 const d = document
 
 export default function calculator() {
+    // Importar elementos del DOM y declarar variables
     const display = d.querySelector('.display'),
         clear = d.getElementById('clear'),
         equals = d.getElementById('equals'),
@@ -8,9 +9,12 @@ export default function calculator() {
         numbers = d.querySelectorAll('#number'),
         operators = d.querySelectorAll('#operator')
 
-    let currentNumber = '0',
-        stack = [],
-        operator = null,
+    // Variables para almacenar el número actual, la pila y el operador actual
+    let currentNumber = '0', // Almacena el número actual en pantalla
+        stack = [], // Almacena los números en la pila para cálculos
+        operator = null, // Almacena el operador actual
+
+        // Mapeo de operadores para realizar cálculos
         operatorMap = {
             '+': (a, b) => a + b,
             '-': (a, b) => a - b,
@@ -18,11 +22,13 @@ export default function calculator() {
             '/': (a, b) => b === 0 ? "Error" : a / b
         }
 
+    // Función para actualizar el contenido del display
     function updateDisplay() {
         return display.textContent = currentNumber
         // parseFloat(currentNumber).toLocaleString('en') -> Esto es para que se muestre con comas directamente
     }
 
+    // Función para borrar todo y reiniciar la calculadora
     function clearDisplay() {
         currentNumber = '0';
         stack = [];
@@ -30,198 +36,108 @@ export default function calculator() {
         updateDisplay();
     }
 
+    // Función para agregar números al número actual
     function appendNumbers(number) {
+        // Si el número actual es 0, reemplaza por el nuevo número
         if (currentNumber === '0') {
             currentNumber = number
         }
         else {
+            // Si no, agrega el dígito al número actual
             currentNumber += number
         }
         updateDisplay()
     }
 
+    // Función para aplicar operadores y realizar cálculos
     function applyOperator(selectedOperator) {
+        // Si el operador seleccionado es '%', dividir el número actual por 100
         if (selectedOperator === '%') {
             const num = parseFloat(currentNumber.replace(",", "."));
             currentNumber = (num / 100).toString().replace(".", ",");
             updateDisplay();
             return;
         }
+
+        // Si el operador seleccionado es '+/-', cambiar el signo del número actual
         else if (selectedOperator === '+/-') {
             const num = parseFloat(currentNumber.replace(",", "."));
             currentNumber = (num * -1).toString().replace(".", ",");
             updateDisplay();
             return;
         }
+
+        // Si hay un número en el display, añadirlo a la pila y reiniciar el número actual
         if (currentNumber !== '') {
             stack.push(parseFloat(currentNumber));
             console.log(currentNumber)
             currentNumber = '';
             console.log(stack)
         }
+
+        // Si hay suficientes elementos en la pila para calcular, realizar la operación
         if (stack.length >= 2) {
             const num2 = stack.pop();
             const num1 = stack.pop();
+            // Calcula el resultado usando el operador actual
             const result = operatorMap[operator](num1, num2);
+            // Agrega el resultado a la pila y actualiza el número actual y el display
             stack.push(result);
             operator = selectedOperator
             currentNumber = result.toString();
             updateDisplay();
-            currentNumber = '';
+            currentNumber = ''; // Reinicia el número actual para la próxima entrada
         }
         else {
+            // Si no hay suficientes elementos en la pila, establece el operador
             operator = selectedOperator
         }
     }
 
+    // Función para agregar el punto decimal al número actual
     function appendDecimal() {
+        // Si el número actual aún no tiene un punto decimal, agrégalo
         if (!currentNumber.replace(",", ".").includes(".")) {
             currentNumber += dot.textContent
         }
         updateDisplay()
     }
 
-    // function calculates() {
-    //     if (num2 === null) {
-    //         // Si num2 es null, sumamos el valor de num2 a currentNumber
-    //         if (operator === "+") {
-    //             currentNumber = (parseFloat(currentNumber.replace(",", ".")) + num1).toString();
-    //         } else if (operator === "-") {
-    //             currentNumber = (parseFloat(currentNumber.replace(",", ".")) - num1).toString();
-    //         } else if (operator === "x") {
-    //             currentNumber = (parseFloat(currentNumber.replace(",", ".")) * num1).toString();
-    //         } else if (operator === "/") {
-    //             currentNumber = (parseFloat(currentNumber.replace(",", ".")) / num1).toString();
-    //         }
-    //     }
-    //     else {
-    //         num2 = parseFloat(currentNumber.replace(",", "."))
-    //         let result
-    //         switch (operator) {
-    //             case "+":
-    //                 result = num1 + num2
-    //                 break;
-
-    //             case "-":
-    //                 result = num1 - num2
-    //                 break;
-
-    //             case "x":
-    //                 result = num1 * num2
-    //                 break;
-
-    //             case "/":
-    //                 if (parseFloat(num2) === 0) {
-    //                     currentNumber = "Error"
-    //                     updateDisplay()
-    //                     return
-    //                 }
-    //                 result = num1 / num2
-    //                 break;
-
-    //             default:
-    //                 return
-    //         }
-    //         currentNumber = result.toString()
-    //         num1 = parseFloat(currentNumber.replace(",", "."));
-    //     }
-    //     updateDisplay()
-
-    //     // operators.forEach(btn => {
-    //     //     btn.addEventListener('click', e => {
-    //     //         console.log("valor display:", display.textContent)
-
-    //     //         num1 = parseInt(display.textContent)
-    //     //         console.log("Numero 1:", num1)
-
-    //     //         op = btn.textContent;
-    //     //         if (op === "+/-") {
-    //     //             display.textContent = num1 * -1
-    //     //         }
-    //     //         else if (op === "%") {
-    //     //             num1 = parseFloat(display.textContent)
-    //     //             display.textContent = num1 /= 100
-    //     //             console.log("valor display en %:", display.textContent)
-    //     //             console.log("Numero 1 en %:", num1)
-    //     //         }
-    //     //         else {
-    //     //             display.textContent = '0'
-    //     //         }
-    //     //     });
-    //     // });
-    // }
-
+    // Evento al hacer clic en el botón de borrar
     equals.addEventListener("click", e => {
-        // calculates()
+        // Agregar el número actual a la pila si hay algo en él
         if (currentNumber !== '') {
             stack.push(parseFloat(currentNumber));
+            console.log(stack)
             currentNumber = '';
         }
+
+        // if (operator !== null) { }
+
+        // Realizar el cálculo si hay un operador y suficientes elementos en la pila
         if (operator !== null && stack.length >= 2) {
             const num2 = stack.pop();
             const num1 = stack.pop();
+            // Calcula el resultado usando el operador actual
             const result = operatorMap[operator](num1, num2);
+            // Agrega el resultado a la pila y actualiza el display
             stack.push(result);
             currentNumber = result.toString();
             updateDisplay();
-            stack = [];
+            stack = []; // Reinicia la pila para la próxima operación
         }
     })
+
+    // Evento al hacer clic en el botón de borrar
     clear.addEventListener('click', clearDisplay)
+
+    // Eventos al hacer clic en los botones de números y operadores
     numbers.forEach(btn => btn.addEventListener("click", e => appendNumbers(btn.textContent)))
     operators.forEach(btn => btn.addEventListener("click", e => applyOperator(btn.textContent)))
+
+    // Evento al hacer clic en el botón de punto decimal
     dot.addEventListener("click", appendDecimal)
+
+    // Actualizar el contenido del display al cargar la página
     updateDisplay()
 }
-
-// equals.addEventListener("click", e => {
-        //     // if (op.length > 0) {
-        //     //     console.log("Hola")
-        //     //     let result
-        //     //     let num2 = parseInt(display.textContent)
-
-        //     //     console.log("Display:", display.textContent)
-        //     //     console.log("Numero 2:", num2)
-
-        //     //     switch (op) {
-        //     //         case "+":
-        //     //             result = num1 + num2
-        //     //             break;
-
-        //     //         case "-":
-        //     //             result = num1 - num2
-        //     //             break;
-
-        //     //         case "x":
-        //     //             result = num1 * num2
-        //     //             break;
-
-        //     //         case "/":
-        //     //             result = num1 / num2
-        //     //             break;
-
-        //     //         default:
-        //     //             alert("Operador inválido")
-        //     //     }
-        //     //     display.textContent = result.toString()
-        //     //     console.log("Resultado:", result)
-        //     //     op = ''
-        //     // }
-        //     // else {
-        //     //     display.textContent
-        //     // }
-        //     let result
-        //     let num2 = parseInt(display.textContent)
-
-        //     console.log("Display:", display.textContent)
-        //     console.log("Numero 2:", num2)
-
-        //     if (result === Infinity) {
-        //         display.textContent = "Error"
-        //     }
-        //     else {
-        //         display.textContent = result.toString()
-        //         console.log("Resultado:", result)
-        //         // op = ''
-        //     }
-        // });
